6abf8e3037d813f3d7d18c4b3e04155aa51eeaa1
CSITEd Experts <csitedexperts@gmail.com>
2019-01-09 13:11:34 -0500

    Initial commit with the TestProgram.java

---
+++ src/TestProgram.java
@@ -0,0 +1,11 @@
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hello = "Hello Class\nWelcome to the CSE1322 course"; 
+		System.out.println(hello);
+		
+	}
+
+}
