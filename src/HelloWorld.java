74e78825b614e68e658a49705cbc2fbb0c694133
CSITEd Experts <csitedexperts@gmail.com>
2019-01-11 13:09:39 -0500

    Renamed TestProgram to HelloWorld and ...

---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hello = "Hello Class\nWelcome to the CSE1322 course"; 
+		System.out.println(hello);
+//		char ch = 'A';
+		
+		int x = 4;
+		byte b = 120;
+		short s = 3412;
+		long l = 45345;
+		System.out.println(x);
+		System.out.println(b);
+		
+		double d = 23.50;
+		System.out.println(d);
+		float f = 45.50f;
+		System.out.println(d);
+		
+		boolean bl = true; 
+		System.out.println(!bl);
+		
+		int ch = 98;	
+
+		System.out.println(ch);
+		System.out.println((char)ch);
+		System.out.println(ch);
+		
+	
+	}
+
+}
